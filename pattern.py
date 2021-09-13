f7cc8408645a7c3a37faa6810273a4f9a51ee11d
Amit Verma <vermamit89@gmail.com>
2021-09-13 23:01:06 +0530

    Number-Pattern-problems

---
+++ pattern.py
@@ -0,0 +1,63 @@
+# Pattern #1: Simple Number Triangle Pattern
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i, end=' ')
+#     print('')
+
+
+# Pattern #2: Inverted Pyramid of Numbers
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print((6-i),end=' ')
+#     print('')
+
+ 
+# Pattern #3: Half Pyramid Pattern of Numbers
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j,end=' ')
+#     print('')
+
+
+# Pattern #4: Inverted Pyramid of Descending Numbers
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print(i,end=' ')
+#     print('')
+
+
+# Pattern #5: Inverted Pyramid of the Same Digit
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print('5',end=' ')
+#     print('')
+
+
+# Pattern #6: Reverse Pyramid of Numbers
+# for i in range(1,6):
+#     for j in range(i,0,-1):
+#         print(j,end=' ')
+#     print('')
+
+
+# Pattern #7: Inverted Half Pyramid Number Pattern
+# for i in range(6,1,-1):
+#     for j in range(i):
+#         print(j,end=' ')
+#     print('')
+
+
+# Pattern #8: Pyramid of Natural Numbers Less Than 10
+# index=1
+# i=1
+# while index<=6:
+#     j=0
+#     while j<i:
+#         print(index,end=' ')
+#         j+=1
+#         index+=1
+#     print('')
+#     i+=2
+    
+
+
